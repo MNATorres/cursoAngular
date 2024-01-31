bdf1e9db139a677086dab1e7c1150352749faad9
matias.torres <mtri21019@gmail.com>
2024-01-31 12:38:04 -0300

    creando un signal dinámico

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,38 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 
 @Component({
   selector: 'app-labs',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './labs.component.html',
-  styleUrl: './labs.component.css'
+  styleUrl: './labs.component.css',
 })
 export class LabsComponent {
   welcome = 'Hola!';
-  tasks = ['Instalar el Angular CLI', 'Creart Proyecto', 'Crear Componentes', 'Crear servicio'];
-  name = 'Matias'
-  age = 29
+  tasks = [
+    'Instalar el Angular CLI',
+    'Creart Proyecto',
+    'Crear Componentes',
+    'Crear servicio',
+  ];
+  name = signal('Matias');
+  age = 29;
   disabled = true;
-  img = 'https://www.lavoz.com.ar/resizer/zU6FRuN30kNbMi1SWMTpuJ0pMLg=/980x640/smart/filters:quality(75):format(webp)/cloudfront-us-east-1.images.arcpublishing.com/grupoclarin/GI4DEODEGNSTAMJYGZRWKN3FGQ.jpg'
+  img =
+    'https://www.lavoz.com.ar/resizer/zU6FRuN30kNbMi1SWMTpuJ0pMLg=/980x640/smart/filters:quality(75):format(webp)/cloudfront-us-east-1.images.arcpublishing.com/grupoclarin/GI4DEODEGNSTAMJYGZRWKN3FGQ.jpg';
 
   person = {
     name: 'Nehuen',
     age: 29,
-    avatar: 'https://avatars.githubusercontent.com/u/96670695?v=4'
-  }
+    avatar: 'https://avatars.githubusercontent.com/u/96670695?v=4',
+  };
 
   clickHandler() {
-    alert("Me estan haciendo click")
+    alert('Me estan haciendo click');
   }
 
-  changeHandler(event:Event) {
-    console.log(event)
+  changeHandler(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    this.name.set(newValue)
   }
 
   keydownHandler(event: KeyboardEvent) {
-    const input = event.target as HTMLInputElement
-    console.log(input.value)
+    const input = event.target as HTMLInputElement;
+    console.log(input.value);
   }
 }
